6e50e5e1c5653c923a5a207655edc1f92d819a93
Tharshini98 <70389909+Tharshini98@users.noreply.github.com>
2025-08-03 09:28:30 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -13,6 +13,11 @@ app.use(express.json());
 
 app.use('/api/auth', authRoutes);
 
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+
+
 app.listen(process.env.PORT, () => {
   console.log(`Server running on port ${process.env.PORT}`);
 });
